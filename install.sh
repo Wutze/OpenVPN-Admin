8508dee15fdf16ab7825564450215ed37eb9a3bd
Wutze <micro@mail.home>
2020-03-22 15:41:14 +0100

    code improved, documentation completed and a gimmick added

--- install.sh
+++ install.sh
@@ -13,6 +13,22 @@
 # debug
 #set -x
 
+## short Description install file
+## script explained step by step
+# in the first define variables, load config files
+# setting up the functions
+# then start the script
+# read the arguments, check this, set the install path
+# check installation path and whether group and user exist
+# Inputs all vars with Message Boxes
+# setup mysql > databases, tables, access and first admin to login webfrontend
+# read vars and setting up to create ca-certs
+# create system services
+# create and copy bash scripts for server
+# create and copy files for webfrontend
+# in the last install third party components, setting access rights
+# finnish the script with message
+
 ## Fix Debian 10 Fehler
 export PATH=$PATH:/usr/sbin:/sbin
 
@@ -33,50 +49,58 @@ c=$(( columns / 2 ))
 r=$(( r < 20 ? 20 : r ))
 c=$(( c < 70 ? 70 : c ))
 
-# If the color table file exists,
+# If the color table file exists
 if [[ -f "${coltable}" ]]; then
-	# source it
-	source ${coltable}
+  # source it
+  source ${coltable}
 # Otherwise,
 else
-	# Set these values so the installer can still run in color
-	COL_NC='\e[0m' # No Color
-	COL_LIGHT_GREEN='\e[1;32m'
-	COL_LIGHT_RED='\e[1;31m'
-	COL_BLUE='\e[94m'
-	TICK="[${COL_LIGHT_GREEN}✓${COL_NC}]"
-	CROSS="[${COL_LIGHT_RED}✗${COL_NC}]"
-	INFO="[i]"
-	# shellcheck disable=SC2034
-	DONE="${COL_LIGHT_GREEN} done!${COL_NC}"
+  # Set these values so the installer can still run in color
+  COL_NC='\e[0m' # No Color
+  COL_LIGHT_GREEN='\e[1;32m'
+  COL_LIGHT_RED='\e[1;31m'
+  COL_BLUE='\e[94m'
+  TICK="[${COL_LIGHT_GREEN}✓${COL_NC}]"
+  CROSS="[${COL_LIGHT_RED}✗${COL_NC}]"
+  INFO="[i]"
+  # shellcheck disable=SC2034
+  DONE="${COL_LIGHT_GREEN} done!${COL_NC}"
 	OVER="\\r\\033[K"
 fi
 
 ## Intro with colored Logo
 intro(){
+  NOW=$(date +"%Y")
 	echo -e "${COL_LIGHT_RED}
-■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 ${COL_BLUE}        ◢■◤
       ◢■◤
-    ◢■◤  ${COL_LIGHT_RED}M I C R O - M A D E / H O M E - ${COL_NC}V P N A D M I N${COL_LIGHT_RED} - S E R V E R${COL_BLUE}
-  ◢■◤                                                【ツ】 © 2018-20
-◢■■■■■■■■■■■■■■■■■■■■◤                             ${COL_LIGHT_RED}L   I   N   U   X
-■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■${COL_NC}
+    ◢■◤  ${COL_LIGHT_RED} O P E N V P N - ${COL_NC}W E B A D M I N${COL_LIGHT_RED} - S E R V E R${COL_BLUE}
+  ◢■◤                         【ツ】 © 10.000BC - ${NOW}
+◢■■■■■■■■■■■■■■■■■■■■◤             ${COL_LIGHT_RED}L   I   N   U   X
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■${COL_NC}
 "
 }
 
 # read config.conf
 # you must copy config.conf.example to config.conf and edit this file
 if [[ -f "${config}" ]]; then
-	# source it
-	source ${config}
+  # source it
+  source ${config}
 # Otherwise,
 else
   echo "Missing configuration"
   echo "you must copy config.conf.example to config.conf and edit this file"
+  echo "cp config.conf.example config.conf"
+  echo "to edit this file: nano config.conf"
   exit
 fi
 
+#
+#  name: print_help
+#  @param none
+#  @return message if no options have been specified and exit script
+#  
 print_help () {
   echo -e "./install.sh www_basedir user group"
   echo -e "\tbase_dir: The place where the web application will be put in"
@@ -85,6 +109,7 @@ print_help () {
 }
 
 #
+#  description: intercepts errors and displays them as messages
 #  name: control_box
 #  @param $? + Description
 #  @return Message OK or Exit Script
@@ -99,32 +124,34 @@ control_box(){
   fi
 }
 
-#  
+#
+#  description: formats the notes and messages in an appealing form
 #  name: print_out
 #  @param [1|0|i|d|r] [Text]]
 #  @return formated Text with red cross, green tick, "i"nfo, "d"one Message or need input with "r"
 #  
 print_out(){
-	case "${1}" in
-		1)
-		echo -e " ${TICK} ${2}"
-		;;
-		0)
-		echo -e " ${CROSS} ${2}"
-		;;
-		i)
-		echo -e " ${INFO} ${2}"
-		;;
-		d)
-		echo -e " ${DONE} ${2}"
-		;;
-		r)	read -rsp " ${2}"
-			echo "\n"
-		;;
-	esac
+  case "${1}" in
+    1)
+    echo -e " ${TICK} ${2}"
+    ;;
+    0)
+    echo -e " ${CROSS} ${2}"
+    ;;
+    i)
+    echo -e " ${INFO} ${2}"
+    ;;
+    d)
+    echo -e " ${DONE} ${2}"
+    ;;
+    r)	read -rsp " ${2}"
+	    echo "\n"
+    ;;
+  esac
 }
 
-#  Intercept and display errors
+#
+#  description: Intercept and display errors
 #  name: control_script
 #  @param $?
 #  @return continue script or or exit when error with exit 100
@@ -137,38 +164,43 @@ control_script(){
   fi
 }
 
-# you can only install with root privileges
-# check this
+#
+#  description: you can only install with root privileges, check this
+#  name: check_user
+#  @param $?
+#  @return continue script or or exit when no root user
+#  
 check_user(){
-	# Must be root to install
-	local str="Root user check"
-	if [[ "${EUID}" -eq 0 ]]; then
-		# they are root and all is good
-		print_out 1 "${str}"
-	else
-		print_out 0 "${str}"
-		print_out i "${COL_LIGHT_RED}Script called with non-root privileges${COL_NC}"
-		print_out i "The Installation requires root privileges to install and run"
-		print_out 1 "Installation aborted"
-		exit 1
-	fi
+  # Must be root to install
+  local str="Root user check"
+  if [[ "${EUID}" -eq 0 ]]; then
+    # they are root and all is good
+    print_out 1 "${str}"
+  else
+    print_out 0 "${str}"
+    print_out i "${COL_LIGHT_RED}Script called with non-root privileges${COL_NC}"
+    print_out i "The Installation requires root privileges to install and run"
+    print_out 1 "Installation aborted"
+    exit 1
+  fi
 }
 
+#### Start Script with Out- and Inputs
+## first call funcions
 clear
 intro
 print_out i 'Press enter to continue the VPN-Admin Setup or strg+c to break...'
 print_out r ''
-# Ensure to be root
 check_user
 
-# Ensure there are enought arguments
+# examine the arguments submitted
 if [ "$#" -ne 3 ]; then
   print_help
   exit
 fi
 
 # Ensure there are the prerequisites
-for i in openvpn mysql php yarn node unzip wget sed; do
+for i in openvpn mysql php yarn node unzip wget sed route; do
   which $i > /dev/null
   if [ "$?" -ne 0 ]; then
     echo "Miss $i"
@@ -203,6 +235,7 @@ control_box $? "OpenVPN Port"
 
 db_host=$(whiptail --inputbox "MySQL Host\n(localhost, IP or FQDN)\n\nIf you are using an external database server,\nconfigure it previously so that you can enter a user name and password." 8 78 localhost --title "DB Host" 3>&1 1>&2 2>&3)
 control_box $? "DB-Host"
+
 ## If you are using an external database server
 ## configure it previously so that you can enter a user name and password.
 if [ "$db_host" == localhost ]; then
@@ -226,7 +259,6 @@ control_box $? "Web Admin PW"
 #  @return insert new database, user and setup password
 #  
 set_mysql(){
-
   EXPECTED_ARGS=3
   MYSQL=`which mysql`
   Q1="CREATE DATABASE IF NOT EXISTS $1;"
@@ -255,14 +287,14 @@ control_script "Insert Webadmin User"
 
 print_out 1 "setting up MySQL OK"
 
-print_out i "################## Creating the certificates ##################"
+print_out i "Creating the certificates"
 
 # Get the rsa keys
 cd /opt/
 wget "https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.6/EasyRSA-unix-v3.0.6.tgz"
 tar -xaf "EasyRSA-unix-v3.0.6.tgz"
 mv "EasyRSA-v3.0.6" /etc/openvpn/easy-rsa
-#rm "EasyRSA-unix-v3.0.6.tgz"
+rm "EasyRSA-unix-v3.0.6.tgz"
 
 cd /etc/openvpn/easy-rsa
 ## This vars read from config.conf, see above in this script
@@ -297,20 +329,18 @@ if [[ ! -z $key_cn ]]; then
   export EASYRSA_REQ_CN=$key_cn
 fi
 
-# Init PKI dirs and build CA certs
+
+print_out i "Setup OpenVPN"
+print_out i "Init PKI dirs and build CA certs"
 ./easyrsa init-pki
 ./easyrsa build-ca nopass
-# Generate Diffie-Hellman parameters
+print_out i "Generate Diffie-Hellman parameters"
 ./easyrsa gen-dh
-# Genrate server keypair
+print_out i "Genrate server keypair"
 ./easyrsa build-server-full server nopass
-
-# Generate shared-secret for TLS Authentication
+print_out i "Generate shared-secret for TLS Authentication"
 openvpn --genkey --secret pki/ta.key
-
 print_out 1 "setting up EasyRSA Ok"
-
-print_out i "################## Setup OpenVPN ##################"
 
 # Copy certificates and the server configuration in the openvpn directory
 cp /etc/openvpn/easy-rsa/pki/{ca.crt,ta.key,issued/server.crt,private/server.key,dh.pem} "/etc/openvpn/"
@@ -325,10 +355,9 @@ fi
 nobody_group=$(id -ng nobody)
 sed -i "s/group nogroup/group $nobody_group/" "/etc/openvpn/server.conf"
 
-print_out i "################## Setup firewall ##################"
+print_out i "Setup firewall"
 
-# Make ip forwading and make it persistent
-
+## create systemd Service
 echo "
 
 [Unit]
@@ -345,7 +374,7 @@ WantedBy=default.target
 
 " > /etc/systemd/system/firewall.service
 
-
+## create simple firewall-script
 echo "#/bin/sh
 export PATH=$PATH:/usr/sbin:/sbin
 
@@ -363,15 +392,18 @@ iptables -A FORWARD -i tun0 -o \$primary_nic -j ACCEPT
 iptables -t nat -A POSTROUTING -o \$primary_nic -j MASQUERADE
 iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o \$primary_nic -j MASQUERADE
 iptables -t nat -A POSTROUTING -s 10.8.0.2/24 -o \$primary_nic -j MASQUERADE
+
+# fixes problems with the persistent transmissions e.g. netflix
+iptables -t mangle -o \$primary_nic --insert FORWARD 1 -p tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1400:65495 -j TCPMSS --clamp-mss-to-pmtu
 " > /usr/sbin/firewall.sh
 
 chmod +x /usr/sbin/firewall.sh
 systemctl enable firewall.service
 systemctl start firewall
 
-print_out i "################## Setup web application ##################"
+print_out i "Setup web application"
 
-# Copy bash scripts (which will insert row in MySQL)
+## Copy bash scripts (which will insert row in MySQL)
 cp -r "$base_path/installation/scripts" "/etc/openvpn/"
 chmod +x "/etc/openvpn/scripts/"*
 
@@ -398,14 +430,13 @@ sed -i "s/\$pass = '';/\$pass = '$mysql_user_pass';/" "./include/config.php"
 
 # Replace in the client configurations with the ip of the server and openvpn protocol
 for file in $(find ../ -name client.ovpn); do
-    sed -i "s/remote xxx\.xxx\.xxx\.xxx 443/remote $ip_server $server_port/" $file
-    echo "<ca>" >> $file
-    cat "/etc/openvpn/ca.crt" >> $file
-    echo "</ca>" >> $file
-    echo "<tls-auth>" >> $file
-    cat "/etc/openvpn/ta.key" >> $file
-    echo "</tls-auth>" >> $file
-
+  sed -i "s/remote xxx\.xxx\.xxx\.xxx 443/remote $ip_server $server_port/" $file
+  echo "<ca>" >> $file
+  cat "/etc/openvpn/ca.crt" >> $file
+  echo "</ca>" >> $file
+  echo "<tls-auth>" >> $file
+  cat "/etc/openvpn/ta.key" >> $file
+  echo "</tls-auth>" >> $file
   if [ $openvpn_proto = "udp" ]; then
     sed -i "s/proto tcp-client/proto udp/" $file
   fi
